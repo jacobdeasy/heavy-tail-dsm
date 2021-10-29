2d2ed11d256e4d817b3edd2e6d7bfca2ee626860
jacobdeasy <jd645@cam.ac.uk>
2021-10-29 10:25:31 +0100

    update for experiment runs

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def parse_args_and_config():
     args.log_path = os.path.join(args.exp, 'logs', args.doc)
 
     # parse config file
-    with open(os.path.join('configs', args.config), 'r') as f:
+    with open(args.config, 'r') as f:
         config = yaml.safe_load(f)
     new_config = dict2namespace(config)
 
